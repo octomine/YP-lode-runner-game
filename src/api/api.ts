bd52c1e3bb24dca3c6f4ed21d10f5bdd408b2a5f
octo <jamine61@mail.ru>
2025-10-24 11:28:50 +0300

    remove useless

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,33 +1,5 @@
 import axios/* , { AxiosInstance, CreateAxiosDefaults } */ from 'axios'
 
-// const baseURLLocal = ''
-
-// const instances: Record<string, AxiosInstance> = {}
-
-// const getInstance = (baseURL: string) => {
-//   if (!instances[baseURL]) {
-//     const config: CreateAxiosDefaults = {
-//       baseURL,
-//       timeout: 1000,
-//       withCredentials: true,
-//     }
-//     if (baseURL === baseURLLocal) {
-//       config.responseType = 'blob'
-//     }
-//     instances[baseURL] = axios.create(config)
-//     instances[baseURL].interceptors.response.use(undefined, error => {
-//       if (error.response.status === '401') {
-//         window.location.href = '/#/login'
-//       }
-//       throw error
-//     })
-
-//     setAdapterForSSR(instances[baseURL])
-//   }
-
-//   return instances[baseURL]
-// }
-
 export const apiFile = axios.create({
   baseURL: '',
   responseType: 'blob',
