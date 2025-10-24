1fb48ac3f3eb6c70ddb53e8b5ade253a406046cc
octo <jamine61@mail.ru>
2025-10-24 13:44:40 +0300

    some update

--- src/api/GameApi.ts
+++ src/api/GameApi.ts
@@ -2,5 +2,5 @@ import { AxiosResponse } from 'axios';
 import { apiFile } from './api';
 // это костыль для того чтобы по-быстрому задеплоить, а по хорошему надо использовать перменные окружения
 export class GameAPI {
-  static read = (path: string) => apiFile.get<string, AxiosResponse<Blob>>(`YP-lode-runner-game/game/${path}`)
+  static read = (path: string) => apiFile.get<string, AxiosResponse<Blob>>(`game/${path}`)
 }
